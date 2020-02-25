c1cd0f4d49248e5e6ea0a01e4fc1ea613c9b01d0
Nicholas <nicholas@treetop.us>
2020-02-25 11:33:22 -0500

    Revert "futzing wiyth all sortsa shit to get this working locallly"
    This reverts commit cd48765b75866da13a57da8eee196dad4c40f1c7.

--- lib/example/application.ex
+++ lib/example/application.ex
@@ -4,15 +4,11 @@ defmodule Example.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
-    children =
-      [
-        supervisor(Example.Database, []),
-        supervisor(ExampleWeb.Endpoint, [])
-      ] ++
-        case Application.get_env(:libcluster, :topologies) do
-          nil -> []
-          topologies -> [{Cluster.Supervisor, [topologies, [name: Example.ClusterSupervisor]]}]
-        end
+    children = [
+      supervisor(Cluster.Supervisor, [topologies, [name: Example.ClusterSupervisor]]),
+      supervisor(Example.Database, []),
+      supervisor(ExampleWeb.Endpoint, []),
+    ]
 
     opts = [strategy: :one_for_one, name: Example.Supervisor]
     Supervisor.start_link(children, opts)
--- lib/example/repo.ex
+++ lib/example/repo.ex
@@ -1,7 +1,5 @@
 defmodule Example.Repo do
-  use Ecto.Repo, otp_app: :distillery_example,
-  adapter: Ecto.Adapters.Postgres
-
+  use Ecto.Repo, otp_app: :distillery_example
 
   @doc """
   Dynamically loads the repository url from the
