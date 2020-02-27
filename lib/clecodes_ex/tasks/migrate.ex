4140b50ea3162341ee6ebef8241d31fdfa7d9244
Nicholas <nicholas@treetop.us>
2020-02-27 10:08:49 -0500

    fix module name and alias in migrate task

--- lib/clecodes_ex/tasks/migrate.ex
+++ lib/clecodes_ex/tasks/migrate.ex
@@ -1,15 +1,16 @@
 defmodule ClecodesEx.Tasks.Migrate do
+  alias Distillery.Releases.Config.Providers
   @moduledoc false
 
   def migrate(_args) do
     # Configure
-    Mix.Releases.Config.Providers.Elixir.init(["${RELEASE_ROOT_DIR}/etc/config.exs"])
-    repo_config = Application.get_env(:distillery_example, Example.Repo)
+    Providers.Elixir.init(["${RELEASE_ROOT_DIR}/etc/config.exs"])
+    repo_config = Application.get_env(:distillery_example, ClecodesEx.Repo)
     repo_config = Keyword.put(repo_config, :adapter, Ecto.Adapters.Postgres)
-    Application.put_env(:distillery_example, Example.Repo, repo_config)
+    Application.put_env(:distillery_example, ClecodesEx.Repo, repo_config)
 
     # Start requisite apps
-    IO.puts "==> Starting applications.."
+    IO.puts "==> Starting applications..."
     for app <- [:crypto, :ssl, :postgrex, :ecto, :ecto_sql] do
       {:ok, res} = Application.ensure_all_started(app)
       IO.puts "==> Started #{app}: #{inspect res}"
@@ -17,7 +18,7 @@ defmodule ClecodesEx.Tasks.Migrate do
 
     # Start the repo
     IO.puts "==> Starting repo"
-    {:ok, _pid} = Example.Repo.start_link(pool_size: 1, log: true, log_sql: true)
+    {:ok, _pid} = ClecodesEx.Repo.start_link(pool_size: 1, log: true, log_sql: true)
 
     # Run the migrations for the repo
     IO.puts "==> Running migrations"
@@ -25,11 +26,11 @@ defmodule ClecodesEx.Tasks.Migrate do
     migrations_dir = Path.join([priv_dir, "repo", "migrations"])
 
     opts = [all: true]
-    pool = Example.Repo.config[:pool]
+    pool = ClecodesEx.Repo.config[:pool]
     if function_exported?(pool, :unboxed_run, 2) do
-      pool.unboxed_run(Example.Repo, fn -> Ecto.Migrator.run(Example.Repo, migrations_dir, :up, opts) end)
+      pool.unboxed_run(ClecodesEx.Repo, fn -> Ecto.Migrator.run(ClecodesEx.Repo, migrations_dir, :up, opts) end)
     else
-      Ecto.Migrator.run(Example.Repo, migrations_dir, :up, opts)
+      Ecto.Migrator.run(ClecodesEx.Repo, migrations_dir, :up, opts)
     end
 
     # Shut down
